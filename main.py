da73168f613ce1f307e2d76abd25a1098612fbf2
unknown <messithaihung@gmail.com>
2024-08-25 11:22:24 +0700

    week 10

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox,QDialog , QWidget
-from PyQt6 import uic
+from PyQt6 import uic, QtCore , QtGui , QtWidgets
 from PyQt6.QtCore import QDate , QDateTime
 import sys
 import os
@@ -8,6 +8,8 @@ from model.Account import Account , ListAccount
 from ui_py.MainPage import Ui_MainWindow
 from ui_py.dashboardform import Ui_MainWindow
 from model.Food import cook , food
+from ui_py.ui_sortingpage import Ui_sorting
+
 
 
 
@@ -50,7 +52,126 @@ class EditDialog(QDialog):
         self.L.add_food(cook("Null",self.txtname.text(),self.txtdate.text(),self.txtScore   .text(),self.txtUrl.text()))
         home.callAfterInit()
         self.close()
+class SorTing (QMainWindow,Ui_sorting):
+    def __init__ (self):
+         super().__init__()
+         self.setupUi(self)
+         self.l = food()
+         self.loadDataObjects()
     
+    def loadDataObjects(self):
+         for x in self.l.getAllfood():
+            self.widget_2 = QtWidgets.QWidget(parent=self.scrollAreaWidgetContents)
+            self.widget_2.setMinimumSize(QtCore.QSize(636, 608))
+            self.widget_2.setStyleSheet("background-color: rgb(57, 60, 68)")
+            self.widget_2.setObjectName("widget_2")
+            self.widget_5 = QtWidgets.QWidget(parent=self.widget_2)
+            self.widget_5.setGeometry(QtCore.QRect(10, 10, 611, 295))
+            self.widget_5.setStyleSheet("background-color:rgb(31, 31, 31)")
+            self.widget_5.setObjectName("widget_5")
+            self.label_2 = QtWidgets.QLabel(parent=self.widget_5)
+            self.label_2.setGeometry(QtCore.QRect(60, 0, 482, 281))
+            self.label_2.setMinimumSize(QtCore.QSize(482, 281))
+            self.label_2.setMaximumSize(QtCore.QSize(483, 292))
+            self.label_2.setStyleSheet("background-color:  rgb(57, 60, 68)")
+            self.label_2.setText("")
+            self.label_2.setPixmap(QtGui.QPixmap("ui\\../../../Pictures/Screenshots/Screenshot 2024-06-30 223013.png"))
+            self.label_2.setScaledContents(True)
+            self.label_2.setObjectName("label_2")
+            self.lineEdit_3 = QtWidgets.QLineEdit(parent=self.widget_2)
+            self.lineEdit_3.setGeometry(QtCore.QRect(100, 460, 231, 51))
+            font = QtGui.QFont()
+            font.setPointSize(9)
+            self.lineEdit_3.setFont(font)
+            self.lineEdit_3.setStyleSheet("background-color:rgb(31, 31, 31);\n"
+    "color: rgb(255, 255, 255);")
+            self.lineEdit_3.setObjectName("lineEdit_3")
+            self.label_16 = QtWidgets.QLabel(parent=self.widget_2)
+            self.label_16.setGeometry(QtCore.QRect(120, 390, 21, 21))
+            font = QtGui.QFont()
+            font.setPointSize(19)
+            self.label_16.setFont(font)
+            self.label_16.setText("")
+            self.label_16.setPixmap(QtGui.QPixmap("D:/download/star.png"))
+            self.label_16.setScaledContents(True)
+            self.label_16.setObjectName("label_16")
+            self.label_18 = QtWidgets.QLabel(parent=self.widget_2)
+            self.label_18.setGeometry(QtCore.QRect(140, 390, 21, 21))
+            font = QtGui.QFont()
+            font.setPointSize(19)
+            self.label_18.setFont(font)
+            self.label_18.setText("")
+            self.label_18.setPixmap(QtGui.QPixmap("D:/download/star.png"))
+            self.label_18.setScaledContents(True)
+            self.label_18.setObjectName("label_18")
+            self.label_7 = QtWidgets.QLabel(parent=self.widget_2)
+            self.label_7.setGeometry(QtCore.QRect(100, 390, 21, 21))
+            font = QtGui.QFont()
+            font.setPointSize(19)
+            self.label_7.setFont(font)
+            self.label_7.setText("")
+            self.label_7.setPixmap(QtGui.QPixmap("D:/download/star.png"))
+            self.label_7.setScaledContents(True)
+            self.label_7.setObjectName("label_7")
+            self.pushButton_book_thitkho_2 = QtWidgets.QPushButton(parent=self.widget_2)
+            self.pushButton_book_thitkho_2.setGeometry(QtCore.QRect(290, 530, 71, 61))
+            font = QtGui.QFont()
+            font.setPointSize(1)
+            self.pushButton_book_thitkho_2.setFont(font)
+            self.pushButton_book_thitkho_2.setText("")
+            #self.pushButton_book_thitkho_2.setIcon(icon1)
+            self.pushButton_book_thitkho_2.setIconSize(QtCore.QSize(100, 70))
+            self.pushButton_book_thitkho_2.setObjectName("pushButton_book_thitkho_2")
+            self.label_17 = QtWidgets.QLabel(parent=self.widget_2)
+            self.label_17.setGeometry(QtCore.QRect(30, 370, 61, 51))
+            font = QtGui.QFont()
+            font.setPointSize(19)
+            self.label_17.setFont(font)
+            self.label_17.setText("")
+            self.label_17.setPixmap(QtGui.QPixmap("D:/download/like.png"))
+            self.label_17.setScaledContents(True)
+            self.label_17.setObjectName("label_17")
+            self.pushButton_recipe_Thitkho_2 = QtWidgets.QPushButton(parent=self.widget_2)
+            self.pushButton_recipe_Thitkho_2.setGeometry(QtCore.QRect(200, 530, 71, 61))
+            font = QtGui.QFont()
+            font.setPointSize(12)
+            self.pushButton_recipe_Thitkho_2.setFont(font)
+            self.pushButton_recipe_Thitkho_2.setText("")
+           # self.pushButton_recipe_Thitkho_2.setIcon(icon)
+            self.pushButton_recipe_Thitkho_2.setIconSize(QtCore.QSize(100, 60))
+            self.pushButton_recipe_Thitkho_2.setObjectName("pushButton_recipe_Thitkho_2")
+            self.label_15 = QtWidgets.QLabel(parent=self.widget_2)
+            self.label_15.setGeometry(QtCore.QRect(40, 460, 51, 61))
+            font = QtGui.QFont()
+            font.setPointSize(19)
+            self.label_15.setFont(font)
+            self.label_15.setText("")
+            self.label_15.setPixmap(QtGui.QPixmap("D:/download/comment.png"))
+            self.label_15.setScaledContents(True)
+            self.label_15.setObjectName("label_15")
+            self.label_13 = QtWidgets.QLabel(parent=self.widget_2)
+            self.label_13.setGeometry(QtCore.QRect(160, 390, 21, 21))
+            font = QtGui.QFont()
+            font.setPointSize(19)
+            self.label_13.setFont(font)
+            self.label_13.setText("")
+            self.label_13.setPixmap(QtGui.QPixmap("D:/download/star.png"))
+            self.label_13.setScaledContents(True)
+            self.label_13.setObjectName("label_13")
+            self.label_19 = QtWidgets.QLabel(parent=self.widget_2)
+            self.label_19.setGeometry(QtCore.QRect(130, 320, 401, 31))
+            font = QtGui.QFont()
+            font.setPointSize(19)
+            self.label_19.setFont(font)
+            self.label_19.setStyleSheet("color: rgb(255, 255, 255);")
+            self.label_19.setObjectName("label_19")
+            self.pushButton_2 = QtWidgets.QPushButton(parent=self.widget_2)
+            self.pushButton_2.setGeometry(QtCore.QRect(520, 520, 93, 81))
+            self.pushButton_2.setText("")
+            #self.pushButton_2.setIcon(icon2)
+            self.pushButton_2.setIconSize(QtCore.QSize(80, 70))
+            self.pushButton_2.setObjectName("pushButton_2")
+            self.horizontalLayout_2.addWidget(self.widget_2)
 class HomeMenuDashboard(QMainWindow,Ui_MainWindow):
     def __init__ (self):
         super().__init__()
@@ -171,19 +292,6 @@ class MainPage(QMainWindow):
       def ShowAddminPage(self):
         home.show()
         self.close()
-          
-
-class SorTing(QMainWindow):
-      def __init__(self):
-         super().__init__()
-         uic.loadUi("ui/sortingpage.ui",self)
-           # ====== Kết nối sự kiện ở đây
-         self.pushButton_back.clicked.connect(self.ShowMainPage)
-
-      def ShowMainPage(self):
-          mainPage.show()
-          self.close()
-
         
 
 
